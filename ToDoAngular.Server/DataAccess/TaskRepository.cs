08f2884e7b2a9aa977c0388a494d26845527ed8c
IronUmbrellal <irinafutruk932@gmail.com>
2026-08-02 23:52:51 +0300

    interface and repository, which is not done yet

---
+++ ToDoAngular.Server/DataAccess/TaskRepository.cs
@@ -0,0 +1,48 @@
+﻿using ToDoAngular.Server.DataAccess;
+using ToDoAngular.Server.Interfaces;
+using Microsoft.EntityFrameworkCore;
+namespace ToDoAngular.Server.DataAccess
+
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private readonly AppDbContext _context;
+        public TaskRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<ToDoTask>> GetAllTasksAsync()
+        {
+            return await _context.ToDoTasks.ToListAsync();
+        }
+
+        public async Task<ToDoTask?> GetTaskByIdAsync(int id)
+        {
+            return await _context.ToDoTasks.FindAsync(id);
+        }
+
+        public async Task<ToDoTask> AddTaskAsync(ToDoTask task)
+        {
+            _context.ToDoTasks.Add(task);
+            await _context.SaveChangesAsync();
+            return task;
+        }
+
+        public async Task UpdateTaskAsync(ToDoTask task)
+        {
+            _context.ToDoTasks.Update(task);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteTaskAsync(int id)
+        {
+            var task = await _context.ToDoTasks.FindAsync(id);
+            if (task != null)
+            {
+                _context.ToDoTasks.Remove(task);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ ToDoAngular.Server/Interfaces/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using ToDoAngular.Server.DataAccess;
+namespace ToDoAngular.Server.Interfaces
+
+{
+    public interface ITaskRepository
+    {
+        Task<IEnumerable<ToDoTask>> GetAllTasksAsync();
+        Task<ToDoTask?> GetTaskByIdAsync(int id);
+        Task<ToDoTask> AddTaskAsync(ToDoTask task);
+        Task UpdateTaskAsync (ToDoTask task);
+        Task DeleteTaskAsync (int id);
+    }
+}
